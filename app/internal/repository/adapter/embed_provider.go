8ac074adcac5e470e5291657a4896e98ae5ec0fd
Andrew <master@andrews-mbp.lan>
2026-03-22 23:52:43 +0300

    fix: program id patcher

--- app/internal/repository/adapter/embed_provider.go
+++ app/internal/repository/adapter/embed_provider.go
@@ -2,6 +2,7 @@ package adapter
 
 import (
 	"bytes"
+	"encoding/binary"
 	_ "embed"
 	"fmt"
 
@@ -26,7 +27,42 @@ func PatchedProgramBinary(programID string) ([]byte, error) {
 
 	patched := make([]byte, len(embeddedProgram))
 	copy(patched, embeddedProgram)
-	copy(patched[idx:idx+32], pk.Bytes())
+
+	pkBytes := pk.Bytes()
+
+	copy(patched[idx:idx+32], pkBytes)
+
+	patchLddwInstructions(patched, placeholderBytes, pkBytes)
 
 	return patched, nil
+}
+
+func patchLddwInstructions(data []byte, oldID, newID []byte) {
+	var oldChunks [4][8]byte
+	var newChunks [4][8]byte
+	for i := 0; i < 4; i++ {
+		copy(oldChunks[i][:], oldID[i*8:(i+1)*8])
+		copy(newChunks[i][:], newID[i*8:(i+1)*8])
+	}
+
+	for i := 0; i+16 <= len(data); i += 8 {
+		if data[i] != 0x18 {
+			continue
+		}
+
+		immLo := binary.LittleEndian.Uint32(data[i+4 : i+8])
+		immHi := binary.LittleEndian.Uint32(data[i+12 : i+16])
+
+		var loaded [8]byte
+		binary.LittleEndian.PutUint32(loaded[:4], immLo)
+		binary.LittleEndian.PutUint32(loaded[4:], immHi)
+
+		for ci := 0; ci < 4; ci++ {
+			if loaded == oldChunks[ci] {
+				copy(data[i+4:i+8], newChunks[ci][:4])
+				copy(data[i+12:i+16], newChunks[ci][4:])
+				break
+			}
+		}
+	}
 }
